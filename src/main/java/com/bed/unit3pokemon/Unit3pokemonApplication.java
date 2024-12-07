d5a6c700857465a792d30dcc4d3139808082d7e8
HtunThiha <htunthiha.dev@gmail.com>
2024-12-08 02:02:07 +0800

    Initial setup

---
+++ src/main/java/com/bed/unit3pokemon/Unit3pokemonApplication.java
@@ -0,0 +1,13 @@
+package com.bed.unit3pokemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Unit3pokemonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Unit3pokemonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bed/unit3pokemon/Unit3pokemonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bed.unit3pokemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Unit3pokemonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
